6f3b6770e60cf02001fad612b0277a54122770d2
Catalin Stoica <catalinstoica@scan.local>
2016-05-26 17:53:43 +0300

    initial

---
+++ app/src/main/java/com/devtesting/catalinstoica/myapplication/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.devtesting.catalinstoica.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.identity.*;
+
+import io.fabric.sdk.android.Fabric;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "ZL4S5unctj9HXeAhLvwj5LX5g";
+    private static final String TWITTER_SECRET = "txn95tCvj3CofY7HkxKoyXiwsnuKZCDCIG90xv8WH73ZynH6kV";
+
+    private TwitterLoginButton loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        setContentView(R.layout.activity_login);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+
+                Intent mainActivityIntent = new Intent(LoginActivity.this, MainActivity.class);
+                mainActivityIntent.putExtra("username", session.getUserName()); //Optional parameters
+                LoginActivity.this.startActivity(mainActivityIntent);
+
+//                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
---
+++ app/src/main/java/com/devtesting/catalinstoica/myapplication/Timeline.java
@@ -0,0 +1,30 @@
+package com.devtesting.catalinstoica.myapplication;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
+
+public class Timeline extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final UserTimeline userTimeline = new UserTimeline.Builder()
+                .screenName("fabric")
+                .build();
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(userTimeline)
+                .build();
+
+        setListAdapter(adapter);
+
+
+
+
+
+    }
+
+}
